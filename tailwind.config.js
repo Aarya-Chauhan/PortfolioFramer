eb4c55841e0ae8fbdd28fecbffb27b4396ae8627
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-09-04 19:08:07 +0530

    tailwind config, fonts and base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+	container:{
+		center:"true",
+		padding:"15px",	
+	},
+	screens:{
+		sm: '640px',		
+		md: '768px',		
+		lg: '960px',		
+		xl: '1200px',		
+	},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)"
+	},
   	extend: {
+		colors: {
+			primary: '#1c1c22', 
+			accent:{
+				DEFAULT: '#00ff99',
+				hover: '#00e187',
+			},
+		},
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
@@ -16,8 +37,7 @@ module.exports = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
