19dda20a9101cc2c7c0b135995004f8b1d4e1889
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-09-22 20:50:17 +0530

    updating resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,10 +1,43 @@
+"use client";
+
+import {FaHtml5,FaCss3,FaJs,FaReact,FaFigma,FaNodeJs} from'react-icons/fa';
+import {SiTailwindcss,SiNextdotjs} from'react-icons/si';
+
+const about={
+  title: 'About Me',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis autem minima laborum veniam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore.',
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Aarya Chauhan",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+91) 97934 05151",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "1+ Years",
+    },
+    {
+      fieldName: "mail",
+      fieldValue: "aaryachauhan11@gmail.com",
+    },
+  ],
+};
+
+const experience = {
+  icon: ""
+};
 
 
 const ResumePage = () => {
   return (
-    <div>
-      resume page
-    </div>
+    <section>
+      <div className="container mx-auto">
+        resume
+      </div>
+    </section>
   )
 }
 
