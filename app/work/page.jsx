16698e8acf13815786181cd6c7884f260eb8e3f9
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-09-26 21:51:48 +0530

    formatting

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {motion} from 'framer-motion';
 import React,{useState} from 'react';
@@ -120,7 +120,7 @@ const projects = [
 
 
 const Work = () => {
-  const [project,setProject] = useState(projects[0])
+  const [project,setProject] = useState(projects[0]);
   return (
     <motion.section initial={{opacity:1}} animate={{opacity:1, }} className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
       <div className="container mx-auto">
