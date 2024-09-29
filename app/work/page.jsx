e07b663aa50b311408bb67e412b629e33cdd1881
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-09-29 19:28:12 +0530

    adding animation

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -219,7 +219,7 @@ const Work = () => {
   }
   const [project,setProject] = useState(projects[0]);
   return (
-    <motion.section initial={{opacity:1}} animate={{opacity:1, }} className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+    <motion.section initial={{opacity:1}} animate={{opacity:1, transition:{delay:2.4, ease:'easeIn', duration:0.4} }} className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
       <div className="container mx-auto">
         <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
           <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none'>
