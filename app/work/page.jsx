282af5533994029dbc3aed5dfebbeba8a00cfd36
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-09-30 18:06:05 +0530

    completion of work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -8,6 +8,7 @@ import "swiper/css";
 import { Tooltip,TooltipContent,TooltipTrigger,TooltipProvider } from '@radix-ui/react-tooltip';
 import {BsArrowUpRight, BsGithub} from 'react-icons/bs';
 import Image from 'next/image';
+import WorkSliderBtn from '@/components/WorkSliderBtn';
 
 
 const projects = [
@@ -219,7 +220,7 @@ const Work = () => {
   }
   const [project,setProject] = useState(projects[0]);
   return (
-    <motion.section initial={{opacity:1}} animate={{opacity:1, transition:{delay:2.4, ease:'easeIn', duration:0.4} }} className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+    <motion.section initial={{opacity:0}} animate={{opacity:1, transition:{delay:1.4,duration:0.6, ease:'easeIn'} }} className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
       <div className="container mx-auto">
         <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
           <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none'>
@@ -263,11 +264,11 @@ const Work = () => {
             </div>
           </div>
           <div className='w-full xl:w-[50%]'>
-            <Swiper spaceBetween={30} slidesPerView={1} className='xl:h-[460px] mb-12 bg-slate-600' onSlideChange={handleSlideChange}>
+            <Swiper spaceBetween={30} slidesPerView={1} className='xl:h-[540px] mb-12 ' onSlideChange={handleSlideChange}>
               {projects.map((project,index)=>{
                 return <SwiperSlide key={index} className='w-full'>
                   <div className='h-[460px] relative group  flex justify-center items-center bg-pink-50/20'>
-                    <div>
+                    <div className='absolute top-0 bottom-0 w-full h-full bg-black/10 z-10'>
 
                     </div>
                     <div className='relative w-full h-full'>
@@ -276,6 +277,7 @@ const Work = () => {
                   </div>
                 </SwiperSlide>
               })}
+            <WorkSliderBtn containerStyles='flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none' btnStyles='bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all' />
             </Swiper>
           </div>
         </div>
