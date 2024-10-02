e90b4be5e8475a5acfd4ae5e1f766d9b8256c4be
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-10-02 22:39:31 +0530

    styling form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -31,8 +31,13 @@ const ContactPage = () => {
   return (
     <motion.section initial={{opacity:0}} animate={{opacity:1, transition:{delay:1.4, duration:0.6, ease:"easeIn"}}} className="py-6">
       <div className="container mx-auto">
-        <div>
-          <form className="mx-auto"></form>
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">form</form>
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
         </div>
       </div>
     </motion.section>
